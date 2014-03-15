8a349a0e5db4a1e0412c1efc613ce3bdc2dd60b0
maki914 <fujimaki914@gmail.com>
2014-03-15 18:35:56 +0900

    地面、重力を修正

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -4,13 +4,15 @@
 void testApp::setup() {
 	ofSetFrameRate(60);
 	ofSetVerticalSync(true);
+	ofEnableDepthTest();
+	ofEnableSmoothing();
 	ofBackground(255, 255, 255);
 
 	//カメラ設定
 	camera.setDistance(30);
-	camera.setDistance(30);
-	camera.setPosition(ofVec3f(0, -7.f, -10.f));
-	camera.lookAt(ofVec3f(0, 0, 0), ofVec3f(0, -1, 0));
+	//camera.setPosition(ofVec3f(0, -7.f, -10.f));
+	//camera.lookAt(ofVec3f(0, 0, 0), ofVec3f(0, -1, 0));
+	//camera.setFov(60.f);
 
 	//ライティング設定
 	light.enable();
@@ -21,34 +23,38 @@ void testApp::setup() {
 	light.setSpecularColor(ofFloatColor(1.0, 1.0, 1.0));
 
 	world.setup();
+	//world.broadphase();
 	world.enableGrabbing();
+	world.setGravity(ofVec3f(0.f, -9.8f, 0.f));
 	world.enableDebugDraw();
 	world.setCamera(&camera);
+	
 
 	sphere = new ofxBulletSphere();
-	sphere->create(world.world, ofVec3f(0, 0, 0), 0.1, 1.25);
+	sphere->create(world.world, ofVec3f(0, 10, 0), 0.1, 1.25);
 	sphere->add();
 
 	box = new ofxBulletBox();
-	box->create(world.world, ofVec3f(7, 0, 0), .05, .5, .5, .5);
+	box->create(world.world, ofVec3f(7, 10, 0), .05, .5, .5, .5);
 	box->add();
 
 	cone = new ofxBulletCone();
-	cone->create(world.world, ofVec3f(-1, -1, .2), .2, .4, 1.);
+	cone->create(world.world, ofVec3f(-1, 1, .2), .2, .4, 1.);
 	cone->add();
 
 	capsule = new ofxBulletCapsule();
-	capsule->create(world.world, ofVec3f(1, -2, -.2), .4, .8, 1.2);
+	capsule->create(world.world, ofVec3f(1, 2, -.2), .4, .8, 1.2);
 	capsule->add();
 
 	cylinder = new ofxBulletCylinder();
-	cylinder->create(world.world, ofVec3f(0, -2.4, 0), .8, .9, 1.8);
+	cylinder->create(world.world, ofVec3f(0, 2.4, 0), .8, .9, 1.8);
 	cylinder->add();
 
-	ground.create(world.world, ofVec3f(0., 5.5, 0.), 0., 100.f, 1.f, 100.f);
+	ground.create(world.world, ofVec3f(0., 0., 0.), 0., 100.f, 1.f, 100.f);
 	ground.setProperties(.25, .95);
 	ground.add();
 }
+
 
 //--------------------------------------------------------------
 void testApp::update() {
@@ -60,22 +66,25 @@ void testApp::update() {
 void testApp::draw() {
 	glEnable(GL_DEPTH_TEST);
 	camera.begin();
+	ofRotateX(15);
 
 	//地面描画
-	ofSetLineWidth(1.5f);
-	ofSetColor(255, 0, 0);
-	ofLine(-100, 5, 0, 100, 5, 0);
-	ofSetColor(0, 0, 255);
-	ofLine(0, 5, -100, 0, 5, 100);
-	ofSetColor(0, 255, 0);
-	ofLine(0, -100, 0, 0, 100, 0);
-	ofSetColor(200);
+	//ofSetLineWidth(1.5f);
+	//ofSetColor(255, 0, 0);
+	//ofLine(-100, 5, 0, 100, 5, 0);
+	//ofSetColor(0, 0, 255);
+	//ofLine(0, 5, -100, 0, 5, 100);
+	//ofSetColor(0, 255, 0);
+	//ofLine(0, -100, 0, 0, 100, 0);
+	//ofSetColor(200);
 
-	ofSetLineWidth(1.f);
-	for (int i = -50; i <= 50; i++) {
-		ofLine(-50, 5, i, 50, 5, i);
-		ofLine(i, 5, -50, i, 5, 50);
-	}
+	//ofSetLineWidth(1.f);
+	//for (int i = -50; i <= 50; i++) {
+	//	ofLine(-50, 5, i, 50, 5, i);
+	//	ofLine(i, 5, -50, i, 5, 50);
+	//}
+
+	ofDrawGrid(50, 10, true, true, true, true);
 
 
 	ofSetLineWidth(1.f);
--- src/testApp.h
+++ src/testApp.h
@@ -33,3 +33,4 @@ public:
 	ofLight						light;
 		
 };
+
